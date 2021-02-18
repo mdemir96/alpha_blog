eb00134eba99b184eaf01be4ec65e7ba422e00f6
Eray <19mdemir96@gmail.com>
2021-02-18 15:08:54 +0300

    flash and create article added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,22 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])    
     end
 
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))        
+        if @article.save
+            flash[:notice] = "Article was created succesfully."
+            redirect_to article_path(@article)
+
+        else
+            render 'new'        
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     get 'home', to:'pages#home'
     get 'about', to:'pages#about'
 
-    resources :articles, only: [:show]
+    resources :articles, only: [:show, :index , :new, :create]
 end
